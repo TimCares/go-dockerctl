2ca89306713bae4a7d790794391d8da5bc968942
Tim Cares <96243987+TimCares@users.noreply.github.com>
2026-08-16 15:22:19 +0200

    feat(config): added first structure for service_groups in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"errors"
 	"os"
+	"path/filepath"
 	"slices"
 
 	"go.uber.org/zap"
@@ -14,11 +15,19 @@ type RuntimeConfig struct {
 	ActiveEnv  string
 }
 
+const serviceGroupsDefaultDirName = "service-groups"
+
+type ServiceGroup struct {
+	Name string  `yaml:"name"`
+	Path *string `yaml:"path"`
+}
+
 type Config struct {
 	Runtime              RuntimeConfig
-	Name                 string   `yaml:"name"`
-	Envs                 []string `yaml:"envs"`
-	ManageSOPSIdentities bool     `yaml:"manage_sops_identities"`
+	Name                 string         `yaml:"name"`
+	Envs                 []string       `yaml:"envs"`
+	ManageSOPSIdentities bool           `yaml:"manage_sops_identities"`
+	serviceGroups        []ServiceGroup `yaml:"service_groups"`
 }
 
 func getRuntimeConfig(config Config, projectDir string, activeEnv string) (*RuntimeConfig, error) {
@@ -58,6 +67,23 @@ func GetConfig(configFilePath string, projectDir string, activeEnv string) (*Con
 	if err := yaml.Unmarshal(configBody, &cfg); err != nil {
 		zap.L().Error("error parsing config file", zap.Error(err))
 		return nil, err
+	}
+
+	for _, serviceGroup := range cfg.serviceGroups {
+		if serviceGroup.Path == nil || *serviceGroup.Path == "" {
+			defaultServiceGroupPath := filepath.Join(cfg.Runtime.ProjectDir, serviceGroupsDefaultDirName, serviceGroup.Name)
+			serviceGroup.Path = &defaultServiceGroupPath
+		}
+
+		if info, err := os.Stat(*serviceGroup.Path); err != nil {
+			if os.IsNotExist(err) {
+
+			}
+			if !info.IsDir() {
+
+			}
+			// also check general structure that we require for our prjects, but put it into separate validation func
+		}
 	}
 
 	runtimeConfig, err := getRuntimeConfig(cfg, projectDir, activeEnv)
